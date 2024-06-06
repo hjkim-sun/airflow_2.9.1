2cc58c2679db4025450d58bebfe8ab16ca3eb645
hjkim <hjkim_sun@naver.com>
2024-06-06 13:58:56 +0900

    ch9 내용 upload

---
+++ files/sqls/create_seoul_bike_hist.sql
@@ -0,0 +1,9 @@
+CREATE table seoul_bike_hist(
+	rackTotCnt int,
+	stationName varchar(50),
+	parkingBikeTotCnt int,
+	shared int,
+	stationLatitude numeric,
+	stationLongitude numeric,
+	stationId varchar(10)
+);
