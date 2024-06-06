1573116a1db50e6fc2c80f700e7616730e22e4c1
hjkim <hjkim_sun@naver.com>
2024-06-06 19:47:10 +0900

    ch10 내용 upload

--- dags/dags_python_sensor.py
+++ dags/dags_python_sensor.py
@@ -14,14 +14,16 @@ with DAG(
         import json
         connection = BaseHook.get_connection(http_conn_id)
         url = f'http://{connection.host}:{connection.port}/{endpoint}/1/5/{check_date}'
+        print(f'url: {url}')
         response = requests.get(url)
-        
         contents = json.loads(response.text)
+        print(f'response: {contents}')
         code = contents.get('CODE')
 
         # 에러코드 INFO-200: 해당되는 데이터가 없습니다.
         # 미 갱신시 INFO-200으로 리턴됨
         if code is not None and code == 'INFO-200':
+            print('상태코드: INFO-200, 데이터 미갱신')
             return False
         elif code is None:
             keys = list(contents.keys())
@@ -29,8 +31,10 @@ with DAG(
 
             # 정상 조회 코드 (INFO-000)
             if rslt_code == 'INFO-000':
+                print('상태코드: INFO-000, 데이터 갱신 확인')
                 return True
         else:
+            print('상태코드 불분명')
             return False
 
     sensor_task = PythonSensor(
